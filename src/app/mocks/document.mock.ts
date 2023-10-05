677dfee5b46486479a4eac341c815e8aa0a73fa4
Dejan Milovanovic <dejan.milovanovic@codeup.rocks>
2023-10-05 17:03:21 +0200

    Adding document current step

--- src/app/mocks/document.mock.ts
+++ src/app/mocks/document.mock.ts
@@ -1,43 +1,54 @@
-import { DocumentState, DocumentType } from '@models/document.model';
+import {
+  DocumentState,
+  DocumentStep,
+  DocumentType,
+  IntesaDocument,
+} from '@models/document.model';
 
-export const DOCUMENTS_FOR_REVIEW = [
+export const DOCUMENTS_FOR_REVIEW: IntesaDocument[] = [
   {
     id: 1,
     name: 'Dokument 1',
     type: DocumentType.FOR_REVIEW,
     state: DocumentState.INITIAL,
+    currentStep: DocumentStep.REVIEW,
   },
   {
     id: 2,
     name: 'Long name Dokument',
     type: DocumentType.FOR_REVIEW,
     state: DocumentState.INITIAL,
+    currentStep: DocumentStep.REVIEW,
   },
   {
     id: 3,
     name: 'Dokument 3',
     type: DocumentType.FOR_REVIEW,
     state: DocumentState.INITIAL,
+    currentStep: DocumentStep.REVIEW,
   },
 ];
 
-export const DOCUMENTS_TO_SIGN = [
+export const DOCUMENTS_TO_SIGN: IntesaDocument[] = [
   {
     id: 4,
     name: 'Dokument 1',
     type: DocumentType.FOR_SIGNING,
     state: DocumentState.INITIAL,
+    currentStep: DocumentStep.REVIEW,
   },
   {
     id: 5,
     name: 'Long name Dokument',
     type: DocumentType.FOR_SIGNING,
     state: DocumentState.INITIAL,
+    currentStep: DocumentStep.REVIEW,
   },
   {
     id: 6,
     name: 'Dokument 3',
     type: DocumentType.FOR_SIGNING,
     state: DocumentState.INITIAL,
+    currentStep: DocumentStep.REVIEW,
   },
 ];
--- src/app/models/document.model.ts
+++ src/app/models/document.model.ts
@@ -5,6 +5,12 @@ export enum DocumentState {
   WAITING = 'WAITING',
 }
 
+export enum DocumentStep {
+  REVIEW = 'REVIEW',
+  ACCEPTANCE = 'ACCEPTANCE',
+  SIGNING = 'SIGNING',
+}
+
 export enum DocumentType {
   FOR_REVIEW = 'FOR_REVIEW',
   FOR_SIGNING = 'FOR_SIGNING',
@@ -18,6 +24,7 @@ export enum ProcessState {
 export interface IntesaDocument {
   id: number;
   name: string;
-  state: DocumentState;
-  type: DocumentType;
+  state: DocumentState; // Track different states for a document
+  type: DocumentType; // Determine which kind of document it is
+  currentStep: DocumentStep; // Tracking progress bar step
 }
